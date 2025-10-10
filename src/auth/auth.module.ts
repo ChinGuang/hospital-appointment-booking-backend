8d1a7293c03b3c039bb332d14c35d350abe65530
Chin Guang <ganchinguang0622@gmail.com>
2025-10-10 20:48:25 +0800

    Refactor: Move user-related logic to the users module
    This commit refactors the codebase by moving user-related logic from the auth module to a dedicated users module. This change improves the separation of concerns and makes the codebase more modular and maintainable.
    
    The following changes were made:
    
    - Moved the User entity and enum from the auth module to the users module.
    - Created a new UserModule to encapsulate user-related functionality.
    - Created a UserService to handle user-related business logic.
    - Updated the AuthModule to remove dependencies on the User entity.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [],
   controllers: [AuthController],
   providers: [AuthService],
 })
