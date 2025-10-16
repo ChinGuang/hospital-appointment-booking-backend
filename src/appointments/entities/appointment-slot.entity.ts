d09ec197ad3fb03fabf83ab78247f2f936d43d24
Chin Guang <ganchinguang0622@gmail.com>
2025-10-16 18:18:13 +0800

    Add Appointment entities and enum
    Introduces the `Appointment` and `AppointmentSlot` entities, along with the `AppointmentStatus` enum, to support appointment management functionality.

---
+++ src/appointments/entities/appointment-slot.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Doctor } from '../../doctors/entities/doctor.entity';
+
+@Entity()
+export class AppointmentSlot {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @ManyToOne(() => Doctor, { eager: true, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'doctorId', referencedColumnName: 'id' })
+  doctor: Doctor;
+
+  @Column({ type: 'int' })
+  dayOfWeek: number;
+
+  @Column({ type: 'time' })
+  startTime: string;
+
+  @Column({ type: 'time' })
+  endTime: string;
+}
---
+++ src/appointments/enums/appointment-status.enum.ts
@@ -0,0 +1,5 @@
+export enum AppointmentStatus {
+  SCHEDULED = 1,
+  COMPLETED = 2,
+  CANCELLED = 3,
+}
